81f83b385024c9f405382cce6c391ba4930e3bd9
Joshua Dishong <dishong.joshua@yahoo.com>
2023-10-22 13:28:34 -0600

    Finished player movement

--- Asteroids/Assets/Scripts/PlayerController.cs
+++ Asteroids/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public float thrustSpeed;
     private bool isThrusting;
+    private bool isShooting;
     private Rigidbody2D rBody;
     public float rotationDireciton;
     public float rotationSpeed;
-
+    public GameObject _thrust;
     private void Awake()
     {
         rBody = GetComponent<Rigidbody2D>();
@@ -18,6 +19,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Assigning movement keys
         isThrusting = Input.GetKey("w") || Input.GetKey("up");
         if(Input.GetKey("a") || Input.GetKey("left"))
         {
@@ -31,14 +33,26 @@ public class PlayerController : MonoBehaviour
         {
             rotationDireciton = 0.0f;
         }
+        // Player firing
+        isShooting = Input.GetKeyDown(KeyCode.Space);
+        if (isShooting)
+        {
+
+        }
     }
     private void FixedUpdate()
     {
+        // If holding W, player will thrust
         if (isThrusting)
         {
             rBody.AddForce(this.transform.up * this.thrustSpeed);
+            Sprite.Instantiate(_thrust);
         }
-
+        else if (!isThrusting)
+        {
+            
+        }
+        // If a left or right key is being pressed, adds torque to rotate the player.
         if (rotationDireciton != 0.0f)
         {
             rBody.AddTorque(rotationDireciton * this.rotationSpeed);
